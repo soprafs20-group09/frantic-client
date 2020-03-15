d026acb0d761e473a7790f4b012c6910908cde39
Chester <chesta@kekt.us>
2020-03-15 22:11:23 +0100

    Fixed Font, Montserrat now fully supported without Google Fonts!

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import 'styles/index.scss';
 import App from 'components/App';
 
+import "typeface-montserrat";
+
 ReactDOM.render(<App />, document.getElementById('root'));
