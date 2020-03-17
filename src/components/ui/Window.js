8ca6ed21fe798df788668e5e736f63237a0fb094
Chester <chesta@kekt.us>
2020-03-17 16:37:22 +0100

    Implemented Window Component

--- src/components/ui/Window.js
+++ src/components/ui/Window.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
+import {withRouter} from "react-router-dom";
+import 'styles/ui/Window.scss';
 
+/**
+ * This component serves as a base-container for larger windows.
+ * PROPS:
+ * title: string - the window's title.
+ * width: string (optional) - a custom width property to override the window width.
+ */
 class Window extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="window-container" style={{width: this.props.width}}>
+                <div className="window-header">
+                    <p className="window-title">{this.props.title}</p>
+                </div>
+                <div className="window-content">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
 }
 
-export default Window;
+export default withRouter(Window);
