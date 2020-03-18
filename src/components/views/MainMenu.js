de7c95c580082bb8ab98a191a48269882fdb56d2
Chester <chesta@kekt.us>
2020-03-18 02:27:27 +0100

    Added experimental MainMenu Animations

--- src/components/views/MainMenu.js
+++ src/components/views/MainMenu.js
@@ -2,27 +2,67 @@ import * as React from "react";
 import {withRouter} from "react-router-dom";
 import Logo from 'assets/frantic/logo-hollow.svg';
 import InlineSVG from "react-inlinesvg";
-
+import {animated, Transition} from "react-spring/renderprops";
 import "styles/views/MainMenu.scss";
 import MainMenuItem from "components/ui/MainMenuItem";
 import AppContainer from "components/ui/AppContainer";
 
 class MainMenu extends React.Component {
     render() {
+        const up = {
+            height: 0,
+            opacity: 0,
+            transform: 'translateY(-100%)'
+        };
+        const neutral = {
+            height: '300px',
+            opacity: 1,
+            transform: 'translateY(0%)',
+            marginBottom: '15px'
+        };
+        const down = {
+            height: 0,
+            opacity: 0,
+            transform: 'translateY(20%)',
+            marginbottom: 0
+        };
 
         const itemStyle = {
-          marginBottom: '15px'
+            marginBottom: '15px'
         };
+
+        const menuItems = [
+            <MainMenuItem key="create" to="/create" style={itemStyle}>create</MainMenuItem>,
+            <MainMenuItem key="browse" to="/browse" style={itemStyle}>join</MainMenuItem>
+        ];
 
         return (
             <AppContainer withHelp>
                 <div className="mainmenu-container">
-                    <div className="mainmenu-logo-shadow-container">
-                        <InlineSVG src={Logo} className="mainmenu-logo"/>
-                    </div>
+                    <Transition
+                        items={this.props.children}
+                        from={up}
+                        enter={neutral}
+                        leave={up}
+                        trail={200}
+                    >
+                        {item => style =>
+                            <animated.div className="mainmenu-logo-shadow-container" style={style}>
+                                <InlineSVG src={Logo} className="mainmenu-logo"/>
+                            </animated.div>
+                        }
+                    </Transition>
                     <div className="mainmenu-items-container">
-                        <MainMenuItem to="/create" style={itemStyle}>create</MainMenuItem>
-                        <MainMenuItem to="/browse" style={itemStyle}>join</MainMenuItem>
+                        <Transition
+                            items={menuItems}
+                            keys={item => item.key}
+                            from={down}
+                            enter={neutral}
+                            leave={down}
+                            trail={200}
+                        >
+                            {item => style => <animated.div style={style}>{item}</animated.div>}
+                        </Transition>
                     </div>
                 </div>
             </AppContainer>
