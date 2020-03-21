7907f472020f74b49b696e70eed6236ead3999d6
Chester <chesta@kekt.us>
2020-03-21 19:04:29 +0100

    Added first Draft of LobbyList

---
+++ src/components/ui/LobbyList.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import "styles/ui/LobbyList.scss";
+import {animated, Transition} from "react-spring/renderprops";
+
+/**
+ * Renders a list of lobbies.
+ * PROPS:
+ * lobbies: array of objects - the list of lobbies to display. lobbies must be of the following structure:
+ *  -name: string
+ *  -creator: string
+ *  -players: string
+ *  -link: string
+ */
+class LobbyList extends Component {
+    render() {
+        const fromStyle = {
+            height: 0,
+            opacity: 0,
+            transform: 'translateX(100%)'
+        };
+        const toStyle = {
+            height: 55,
+            opacity: 1,
+            transform: 'translateX(0%)'
+        };
+
+        return (
+            <div className="lobby-list-container">
+                <div className="lobby-list-caption-container">
+                    <p className="lobby-list-caption c1">Lobby Name</p>
+                    <p className="lobby-list-caption c2">Creator</p>
+                    <p className="lobby-list-caption c3">Players</p>
+                </div>
+                <ul className="lobby-list">
+                    <Transition
+                        items={this.props.lobbies}
+                        keys={lobby => lobby.link}
+                        from={fromStyle}
+                        enter={toStyle}
+                        leave={fromStyle}
+                        trail={200}
+                    >
+                        {lobby => style =>
+                            <LobbyItem
+                                key={lobby.link}
+                                name={lobby.name}
+                                creator={lobby.creator}
+                                players={lobby.players}
+                                link={lobby.link}
+                                style={style}
+                            />
+                        }
+                    </Transition>
+                </ul>
+            </div>
+        );
+    }
+}
+
+/**
+ * Represents a single entry in the lobby list.
+ * PROPS:
+ * name: string
+ * creator: string
+ * players: string
+ * style: object
+ */
+class LobbyItem extends Component {
+    render() {
+        return (
+            <a href={this.props.link}>
+                <animated.li className="lobby-item" style={this.props.style}>
+                    <p className="lobby-item-text c1">{this.props.name}</p>
+                    <p className="lobby-item-text c2">{this.props.creator}</p>
+                    <p className="lobby-item-text c3">{this.props.players}</p>
+                </animated.li>
+            </a>
+        );
+    }
+}
+
+export default LobbyList;
--- src/components/views/JoinLobbyView.js
+++ src/components/views/JoinLobbyView.js
@@ -4,7 +4,12 @@ import AppContainer from "components/ui/AppContainer";
 import {WindowTransition} from "components/ui/Transitions";
 import Window from "components/ui/Window";
 import SearchBar from "components/ui/SearchBar";
+import LobbyList from "components/ui/LobbyList";
 
+/**
+ * Renders a list of available lobbies.
+ *
+ */
 class JoinLobbyView extends Component {
     render() {
         return (
@@ -17,9 +22,43 @@ class JoinLobbyView extends Component {
     }
 
     getMainWindow() {
+        const lobbies = [
+            {
+                name: "ueli's lobby",
+                creator: "ueli",
+                players: "3/8",
+                link: "https://frant.ic/join?id=2103"
+            },
+            {
+                name: "crocodiles",
+                creator: "schnappi",
+                players: "5/8",
+                link: "https://frant.ic/join?id=1234"
+            },
+            {
+                name: "happy place :)",
+                creator: "niceGuyTM",
+                players: "7/8",
+                link: "https://frant.ic/join?id=0124"
+            },
+            {
+                name: "tryhards only!",
+                creator: "DarkKnight98",
+                players: "6/8",
+                link: "https://frant.ic/join?id=3564"
+            },
+            {
+                name: "cats are awesome :3",
+                creator: "Karen",
+                players: "8/8",
+                link: "https://frant.ic/join?id=1398"
+            },
+        ];
+
         return (
             <Window title="Join a Lobby" width="70vw" maxHeight="90vh">
                 <SearchBar withRefresh/>
+                <LobbyList lobbies={lobbies}/>
             </Window>
         );
     }
