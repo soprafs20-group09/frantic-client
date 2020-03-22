bc5185c8632a4f271dcd0c0b618b077cd2a8728a
Chester <chesta@kekt.us>
2020-03-22 19:05:00 +0100

    Fixed chat window height in CreateLobbyView

--- src/components/views/CreateLobbyView.js
+++ src/components/views/CreateLobbyView.js
@@ -80,7 +80,7 @@ class CreateLobbyView extends Component {
         return (
             <div className="createlobby-container">
                 <div className="createlobby-column main">
-                    <Window key="LobbyWindow" title={this.state.lobbyName} width="48em" height="41em" maxHeight="90vh">
+                    <Window key="LobbyWindow" title={this.state.lobbyName} width="48em" height="41em">
                         <div className="createlobby-window-container">
                             <div className="createlobby-window-column">
                                 <Header>Settings</Header>
