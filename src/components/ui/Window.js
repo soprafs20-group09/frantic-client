33ccdde603916167072863155926918e6a5d9c57
Chester <chesta@kekt.us>
2020-03-22 18:00:23 +0100

    Added scrolling to Window body, changed height of CreateLobbyWindow to fixed

--- src/components/ui/Window.js
+++ src/components/ui/Window.js
@@ -7,17 +7,25 @@ import 'styles/ui/Window.scss';
  * PROPS:
  * title: string               - the window's title.
  * width: string (optional)    - a custom width property to override the window width.
+ * height: string (optional)   - a custom height property to override the window height.
  * maxHeight: string(optional) - a custom height property to override the max window height.
  */
 class Window extends Component {
     render() {
         return (
-            <div className="window-container" style={{width: this.props.width, maxHeight: this.props.maxHeight}}>
+            <div className="window-container"
+                 style={{
+                     width: this.props.width,
+                     height: this.props.height,
+                     maxHeight: this.props.maxHeight
+                 }}>
                 <div className="window-header">
                     <p className="window-title">{this.props.title}</p>
                 </div>
-                <div className="window-content">
-                    {this.props.children}
+                <div className="window-body">
+                    <div className="window-content">
+                        {this.props.children}
+                    </div>
                 </div>
             </div>
         );
