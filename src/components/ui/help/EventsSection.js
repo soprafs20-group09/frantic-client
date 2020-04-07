c6c717e8689b17fc896fd628058434bf2d101c63
sei-nah <sina.ktumhard@uzh.ch>
2020-04-07 17:27:32 +0200

    CardSection, EventsSection: Added overall discription of special cards and events.

--- src/components/ui/help/EventsSection.js
+++ src/components/ui/help/EventsSection.js
@@ -15,6 +15,19 @@ class EventsSection extends Component {
         return (
             <div className="help-section events">
                 <Header>Events</Header>
+                <p>
+                    Events are activated when a Black Card is
+                    played. When that happens, a random event is revealed and gets executed. Every event can only be activated once per round.
+                    <br/>
+                    The first person to be affected is always the person on the right hand side of the
+                    player of the Black Card. If more than one player are the potential subject of the Event Card, it affects the first person
+                    who is in counter-clockwise direction closest to the player of
+                    the Black Card. Unless it is declared otherwise.
+                    <br/>
+                    <th>Important:</th> The current round will not be over, until the event
+                    has been executed completely. And even if a player loses all
+                    of his hand cards due to an event, “Nice Try“ can be played!
+                </p>
                 {eventHelps}
             </div>
         );
