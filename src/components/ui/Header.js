d05ada3b0036e34a995dd71a8a5138eebf6b5ac3
Chester <chesta@kekt.us>
2020-04-19 11:36:45 +0200

    Separator, Header: new component to separate and show steps, added new sizes

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -6,13 +6,18 @@ import 'styles/ui/UiElements.scss';
  * A simple header component.
  * PROPS:
  * width: string    - a width override.
+ * size: string     - normal, small
  */
 class Header extends Component {
     render() {
         return (
-            <p className="header" style={{width: this.props.width}}>{this.props.children}</p>
+            <p className={"header " + this.props.size} style={{width: this.props.width}}>{this.props.children}</p>
         );
     }
 }
+
+Header.defaultProps = {
+    size: 'normal'
+};
 
 export default Header;
---
+++ src/components/ui/Separator.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import "styles/ui/UiElements.scss";
+
+/**
+ * Shows a horizontal numerated separator.
+ * PROPS:
+ * step: number (optional) - displays a step nr. if given
+ * children: string        - the step title.
+ */
+class Separator extends Component {
+    render() {
+        return (
+            <div className="step-container">
+                {(this.props.step !== null) && <div className="step-number">{this.props.step}</div>}
+                <span className="step-separator"/>
+                <h3 className="step-title">{this.props.children}</h3>
+                <span className="step-separator"/>
+            </div>
+        );
+    }
+}
+
+Separator.defaultProps = {
+  step: null
+};
+
+export default Separator;
