65102f1f4f76c7c2ce0019d9b2ca16d98e011c13
Chester <chesta@kekt.us>
2020-04-19 16:23:01 +0200

    GenericColorPicker: new picker for the rest of actions (counterattack & co.)

---
+++ src/components/ui/pickers/GenericColorPicker.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import "styles/ui/pickers/ConcretePickers.scss";
+import ToolWindow from "components/ui/ToolWindow";
+import Separator from "components/ui/Separator";
+import ColorNumberPicker from "components/ui/pickers/parts/ColorNumberPicker";
+import Button from "components/ui/Button";
+
+class GenericColorPicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {color: null};
+    }
+
+    render() {
+        return (
+            <ToolWindow
+                noScroll
+                title={this.props.title}
+            >
+                <div className="concrete-picker-container">
+                    <Separator>
+                        Make your wish
+                    </Separator>
+                    <ColorNumberPicker
+                        onSelectionChange={c => this.handleColorChange(c)}
+                    />
+                    <div className="concrete-picker-spacer"/>
+                    <Button
+                        width="10em"
+                        disabled={this.state.color === null}
+                        onClick={() => this.handleFinish()}
+                    >
+                        Done
+                    </Button>
+                </div>
+            </ToolWindow>
+        );
+    }
+
+    handleFinish() {
+        if (this.props.onFinish()) {
+            this.props.onFinish({color: this.state.color});
+        }
+    }
+
+    handleColorChange(color) {
+        this.setState({color: color});
+    }
+}
+
+export default GenericColorPicker;
