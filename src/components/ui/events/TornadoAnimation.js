74d59fc4d9f4c809f15ef0dc689efa46bbccef7a
Chester <chesta@kekt.us>
2020-04-25 12:52:40 +0200

    TornadoAnimation: first working version

--- src/components/ui/events/TornadoAnimation.js
+++ src/components/ui/events/TornadoAnimation.js
@@ -5,6 +5,15 @@ class TornadoAnimation extends Component {
     render() {
         return (
             <div className="tornado-container">
+                <div className="whirl"/>
+                <div className="whirl"/>
+                <div className="whirl"/>
+                <div className="whirl"/>
+                <div className="whirl"/>
+                <div className="ground"/>
+                <span className="rubble-container top"><span className="rubble"/></span>
+                <span className="rubble-container mid"><span className="rubble"/></span>
+                <span className="rubble-container bot"><span className="rubble"/></span>
             </div>
         );
     }
