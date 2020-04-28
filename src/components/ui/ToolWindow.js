dd243f2ee5bc48f39bc5bf752e4dd11f395e8665
Chesta <chesta@protonmail.com>
2020-04-28 15:03:16 +0200

    ToolWindow: add withClose prop

--- src/components/ui/ToolWindow.js
+++ src/components/ui/ToolWindow.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {withRouter} from "react-router-dom";
+import Icon from "components/ui/Icon";
 
 import 'styles/ui/ToolWindow.scss';
 
@@ -9,6 +10,7 @@ import 'styles/ui/ToolWindow.scss';
  * title: string            - the window's title.
  * style: object (optional) - a custom style property to override the window style.
  * noScroll: bool           - window will hide overflow instead of scrolling
+ * withClose: bool          -
  */
 class ToolWindow extends Component {
     render() {
@@ -16,12 +18,27 @@ class ToolWindow extends Component {
             <div className="toolwindow-container" style={this.props.style}>
                 <div className="toolwindow-header">
                     <p className="toolwindow-title">{this.props.title}</p>
+                    {this.props.withClose &&
+                    <Icon
+                        from="misc"
+                        className="toolwindow-close"
+                        onClick={() => this.handleClose()}
+                    >cross</Icon>}
                 </div>
                 <div className={"toolwindow-content" + (this.props.noScroll ? " no-scroll" : "")}>
                     {this.props.children}
                 </div>
             </div>
         );
+    }
+
+    handleClose() {
+        if (this.props.onClose) {
+            this.props.onClose();
+        }
+        else if (this.props.closeRoute) {
+            this.props.history.push(this.props.closeRoute);
+        }
     }
 }
 
