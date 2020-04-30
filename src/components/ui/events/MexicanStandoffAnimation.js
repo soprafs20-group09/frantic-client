aeb387e203718a2b8491bbbfa403606c7871ff99
Davfon <davide.fontanella@uzh.ch>
2020-04-30 15:32:16 +0200

    MexicanStandoffAnimation: added muzzle flash for each shot

--- src/components/ui/events/MexicanStandoffAnimation.js
+++ src/components/ui/events/MexicanStandoffAnimation.js
@@ -3,6 +3,7 @@ import MexicanStandoffLogo from "assets/icons/mexican-standoff-animation.svg";
 import MexicanStandoffHole1 from "assets/icons/bullet-hole-1.svg";
 import MexicanStandoffHole2 from "assets/icons/bullet-hole-2.svg";
 import MexicanStandoffHole3 from "assets/icons/bullet-hole-3.svg";
+import MexicanStandoffFlash from "assets/icons/muzzle-flash2.svg";
 import InlineSVG from "react-inlinesvg";
 import "styles/ui/events/MexicanStandoffAnimation.scss";
 
@@ -10,11 +11,14 @@ class MexicanStandoffAnimation extends Component {
     render() {
         return (
             <div className="mexican-standoff-container">
-                <div className="revolver-body">
+                <div className="mexican-standoff-body">
                     <InlineSVG src={MexicanStandoffLogo} className="revolver"/>
                     <InlineSVG src={MexicanStandoffHole1} className="shot-hole"/>
                     <InlineSVG src={MexicanStandoffHole2} className="shot-hole"/>
                     <InlineSVG src={MexicanStandoffHole3} className="shot-hole"/>
+                    <InlineSVG src={MexicanStandoffFlash} className="muzzle-flash"/>
+                    <InlineSVG src={MexicanStandoffFlash} className="muzzle-flash muzzle2"/>
+                    <InlineSVG src={MexicanStandoffFlash} className="muzzle-flash muzzle3"/>
                     <div className="smoke"/>
                     <div className="smoke"/>
                     <div className="smoke"/>
