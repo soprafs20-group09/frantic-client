ec77fde0885f5a9e57bae664b1c228b4b2374e66
Chester <chesta@kekt.us>
2020-05-03 14:50:29 +0200

    GameView: potential fix for players clicking things after action-response

--- src/components/views/GameView.js
+++ src/components/views/GameView.js
@@ -575,7 +575,9 @@ class GameView extends Component {
             }
         } catch {
         }
-        this.setState({actionResponse: null});
+        // set the active player to null in order to prevent the user
+        // from clicking anything until next turn starts
+        this.setState({actionResponse: null, activePlayer: null});
     }
 
     // endregion
