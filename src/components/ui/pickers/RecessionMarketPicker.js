a94cca316f5c3a47d94466f5f080e813b3df73e4
Chester <chesta@kekt.us>
2020-05-08 10:40:35 +0200

    RecessionPicker: renamed and added "Market" event functionality

---
+++ src/components/ui/pickers/RecessionMarketPicker.js
@@ -0,0 +1,125 @@
+import React, {Component} from 'react';
+import "styles/ui/pickers/ConcretePickers.scss";
+import ToolWindow from "components/ui/ToolWindow";
+import IconTitle from "components/ui/IconTitle";
+import Separator from "components/ui/Separator";
+import CardPicker from "components/ui/pickers/parts/CardPicker";
+import Button from "components/ui/Button";
+
+/**
+ * Shows a picker that forces the user to select
+ * an amount of cards to discard.
+ * PROPS:
+ * mode: string           - 'recession'/'market': this sets what the picker displays
+ * cards: array           - array of cards
+ * amount: number         - the amount of cards to discard if in 'recession' mode.
+ * onFinish: func(array)  - a function that is called when the user is finished.
+ *                          parameter is an array of indices of the cards the user chose.
+ */
+class RecessionMarketPicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {cards: []};
+    }
+
+    render() {
+        return (
+            <ToolWindow noScroll title={this.getTitle()} style={{maxWidth: '30em'}}>
+                <div className="concrete-picker-container">
+                    <p>
+                        {this.getDescription()}
+                    </p>
+                    <Separator>{this.getSeparatorText()}</Separator>
+                    <CardPicker
+                        cards={this.props.cards}
+                        maxAmount={this.props.mode === 'recession' ? this.props.amount : 1}
+                        onSelectionChange={s => this.handleSelectionChange(s)}
+                    />
+                    <div className="concrete-picker-spacer"/>
+                    <Button
+                        width="10em"
+                        disabled={this.getButtonDisabled()}
+                        onClick={() => this.handleFinish()}
+                    >
+                        Done
+                    </Button>
+                </div>
+            </ToolWindow>
+        );
+    }
+
+    getTitle() {
+        switch (this.props.mode) {
+            case 'recession':
+                return <IconTitle icon="event:recession">Recession</IconTitle>;
+
+            case 'market':
+                return <IconTitle icon="event:market">Market</IconTitle>;
+        }
+    }
+
+    getDescription() {
+        switch (this.props.mode) {
+            case 'recession':
+                return "Due to the current recession," +
+                    ` you need to discard ${this.props.amount} cards!`;
+
+            case 'market':
+                return "The market has opened! Choose a fresh card to buy!";
+
+            default:
+                return null;
+        }
+    }
+
+    getSeparatorText() {
+        switch (this.props.mode) {
+            case 'recession':
+                return `Choose ${this.props.amount} cards to discard`;
+
+            case 'market':
+                return "Pick one card";
+
+            default:
+                return null;
+        }
+    }
+
+    getButtonDisabled() {
+        switch (this.props.mode) {
+            case 'recession':
+                return this.state.cards.length !== this.props.amount;
+
+            case 'market':
+                return this.state.cards.length !== 1;
+
+            default:
+                return true;
+        }
+    }
+
+    handleSelectionChange(selection) {
+        this.setState({cards: selection});
+    }
+
+    handleFinish() {
+        let cards = [];
+
+        for (let c of this.state.cards) {
+            let i = this.props.cards.indexOf(c);
+            if (i >= 0) {
+                cards.push(i);
+            }
+        }
+
+        if (this.props.onFinish) {
+            this.props.onFinish(cards);
+        }
+    }
+}
+
+RecessionMarketPicker.defaultProps = {
+    mode: 'recession'
+};
+
+export default RecessionMarketPicker;
