eea310279db3e16e6c3ce1515978d9789cde4339
Chester <chesta@kekt.us>
2020-05-12 19:18:41 +0200

    AnimationView: add overflow toggle

--- src/components/views/AnimationView.js
+++ src/components/views/AnimationView.js
@@ -4,20 +4,33 @@ import "styles/ui/ingame/OverlayMessage.scss";
 import {withRouter} from "react-router-dom";
 import {EventOverlayTransition} from "components/ui/Transitions";
 import uiUtils from "utils/uiUtils";
+import Button from "components/ui/Button";
 
 class AnimationView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {overflow: 'hidden'}
+    }
+
     render() {
         return (
             <AppContainer>
                 <EventOverlayTransition>
-                    <div className="event-overlay-container" style={{overflow: 'hidden'}}>
+                    <div className="event-overlay-container" style={{overflow: this.state.overflow}}>
                         <div className="event-overlay-content" style={{overflow: 'hidden'}}>
                             {uiUtils.getEventAnimation(this.props.match.params.event)}
                         </div>
                     </div>
                 </EventOverlayTransition>
+                <div style={{position: 'absolute', bottom: '0.5em', right: '0.5em'}}>
+                    <Button style='end-turn' onClick={() => this.toggleOverflow()}>toggle overflow</Button>
+                </div>
             </AppContainer>
         );
+    }
+
+    toggleOverflow() {
+        this.setState({overflow: this.state.overflow === 'hidden' ? 'visible' : 'hidden'});
     }
 }
 
