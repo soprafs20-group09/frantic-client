0f8636c32b80b31d24a4fec754211c660d9a72ff
Chester <chesta@kekt.us>
2020-05-12 19:57:23 +0200

    GameGuard: add loophole for local testing

--- src/components/routing/guards/GameGuard.js
+++ src/components/routing/guards/GameGuard.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import sessionManager from "utils/sessionManager";
 import {Redirect} from "react-router-dom";
+import {isProduction} from "utils/DomainUtils";
 
 class GameGuard extends Component {
     render() {
-        if (sessionManager.inGame) {
+        if (sessionManager.inGame || !isProduction()) {
             return this.props.children;
         }
         return <Redirect to="/"/>;
